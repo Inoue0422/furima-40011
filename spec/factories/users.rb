98314ae937b11604ad83049965eb0b6b1b53c8ce
Inoue0422 <o9045628081@gmail.com>
2023-11-16 17:45:48 +0900

    モデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { 'testuser' }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { '太郎' }
+    last_name_furigana    { 'ヤマダ' }
+    first_name_furigana   { 'タロウ' }
+    date_of_birth         { '1930-01-01' }
+  end
+end
