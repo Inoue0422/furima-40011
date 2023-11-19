a7b05005a5704dca824b43f5d972419696d26c47
Inoue0422 <o9045628081@gmail.com>
2023-11-19 15:37:06 +0900

    商品出品機能修正、item単体テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { 'testuser' }
-    email                 { Faker::Internet.free_email }
+    email                 { Faker::Internet.email }
     password              { "1a#{Faker::Internet.password(min_length: 6)}" }
     password_confirmation { password }
     last_name             { '山田' }
