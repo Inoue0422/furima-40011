014253a9d8956dfa5893ef435dd7a23d0489f8d7
Inoue0422 <o9045628081@gmail.com>
2023-11-19 11:33:56 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,26 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :category_id, presence: true
   validates :condition_id, presence: true
-  validetes :delivery_charge_id, presence: true
+  validates :delivery_charge_id, presence: true
   validates :region_id, presence: true
   validates :delivery_day_id, presence: true
-  validates :price , presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :region
+  belongs_to :delivery_day
 end
