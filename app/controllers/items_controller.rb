f87e91d75e40ad7a95c673107d79efe47710eb9f
Inoue0422 <o9045628081@gmail.com>
2023-11-20 16:27:55 +0900

    商品削除機能itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if user_signed_in? && current_user.id == @item.user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(@item), alert: 'You are not authorized to delete this item.'
+    end
   end
 
   private
