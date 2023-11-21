8d38fd0cb439a0ebb8fb722d9a811455f9cc3def
Inoue0422 <o9045628081@gmail.com>
2023-11-21 22:06:34 +0900

    card.js実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -18,6 +18,6 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_shipping_address_params
-    params.require(:purchase_record_shipping_address).permit(:user_id, :item_id, :post_code, :prefectures_id, :municipalities, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_record_shipping_address).permit(:user_id, :item_id, :post_code, :prefectures_id, :municipalities, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,4 +1,5 @@
 class PurchaseRecord < ApplicationRecord
+  attr_accessor :token
 
   belongs_to :user
   belongs_to :item
